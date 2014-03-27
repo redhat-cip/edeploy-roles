846ca9e47adc33ed6304c07ea8f983d785de5c8b
Emilien Macchi <emilien.macchi@enovance.com>
2014-03-27 17:04:14 +0100

    TEST
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

---
+++ policy-rc.d
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+# Copyright (C) 2013 eNovance SAS <licensing@enovance.com>
+#
+# Author: Frederic Lepied <frederic.lepied@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# Purpose: prevent services to start or do whatever at installation time.
+
+QUIET=
+
+# remove options (see invoke-rc.d man page)
+while :; do
+    case "$1" in
+	--quiet)
+	    QUIET=1
+	    ;&
+	--*)
+	    shift
+	    ;;
+	*)
+	    break
+	    ;;
+    esac
+done
+
+script="$1"
+actions="$2"
+level="$3"
+
+# try to detect that we are called from a package scriptlet
+PPPID=$(ps -p $PPID -o ppid|head -2|tail -1|sed -e 's/^\s*//' -e 's/\s*$//')
+#cat /proc/$PPPID/cmdline|tr '\000' '\n' 1>&2
+set -- $(cat /proc/$PPPID/cmdline|tr '\000' '\n')
+if [ "$2" = -e ]; then
+    SCRIPT="$3"
+else
+    SCRIPT="$2"
+fi
+case "$SCRIPT" in
+    *.postinst|*.preinst|*.postrm|*.prerm)
+	if [ -z "$QUIET" ]; then
+	    echo "policy-rc.d: scriptlet detected: preventing action $actions on $script" 1>&2
+	fi
+	exit 101
+	;;
+    *)
+	if [ -z "$QUIET" ]; then
+	    echo "policy-rc.d: called outside of a scriptlet: action $actions on $script authorized" 1>&2
+	fi
+	;;
+esac
