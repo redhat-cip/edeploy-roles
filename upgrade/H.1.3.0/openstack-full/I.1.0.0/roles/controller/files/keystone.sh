8d602679cb71c250a1dc973e0ab4e375ac5baf66
Emilien Macchi <emilien.macchi@enovance.com>
2014-05-19 11:36:40 +0200

    upgrade to icehouse: prepare keystone database correctly
    in icehouse, keystone db should have "region" table in UTF8 encoding
    format. Ansible playbooks take care of this to support upgrade.
    
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

---
+++ upgrade/H.1.3.0/openstack-full/I.1.0.0/roles/controller/files/keystone.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Copyright (C) 2014 eNovance SAS <licensing@enovance.com>
+#
+# Author: Emilien Macchi <emilien.macchi@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Upgrade keystone database to have UTF8 for "region" table.
+#
+
+set -ex
+
+echo "### 1. Upgrade Keystone database"
+echo "#### FROM : REGION table in non-UTF8"
+echo "#### TO   : REGION table in UTF8"
+
+echo "ALTER TABLE region CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;">keystone.sql
+
+# Perform table upgrade
+mysql -D keystone < keystone.sql
+
+rm keystone.sql
+# keystone.sh ends here
+
