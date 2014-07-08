37b8a39035195981b5b2b0f980225fe4ad4bb18c
Emilien Macchi <emilien.macchi@enovance.com>
2014-07-08 11:15:11 +0200

    I.1.1.0/upgrade/install: add $distro param to be more flexible

--- upgrade/I.1.0.0/I.1.1.0/roles/install/files/edeploy-sync.sh
+++ upgrade/I.1.0.0/I.1.1.0/roles/install/files/edeploy-sync.sh
@@ -23,9 +23,9 @@ set -x
 
 edeploymaster="$1"
 release="$2"
+distro="$3"
 
-#TODO(EmilienM) need to be improved to rsync with the real OS running in deployment
-rsync -av $edeploymaster::ci/all-debian/install/$release/{openstack-full,install-server}*.edeploy* .
+rsync -av $edeploymaster::ci/all-$distro/install/$release/{openstack-full,install-server}*.edeploy* .
 
 # Check that we have the right roles and we are not building new ones
 for role in openstack-full install-server; do
