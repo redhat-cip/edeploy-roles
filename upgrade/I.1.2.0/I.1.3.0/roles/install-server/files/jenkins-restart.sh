ba0e3d8e02b624ee66e130946cfacd1bcb14c90d
Emilien Macchi <emilien.macchi@enovance.com>
2014-12-31 18:40:15 -0500

    [upgrade] use a script to restart jenkins

---
+++ upgrade/I.1.2.0/I.1.3.0/roles/install-server/files/jenkins-restart.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Copyright (C) 2014 eNovance SAS <licensing@enovance.com>
+#
+# Author: Emilien Macchi <emilien.macchi@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# Script that restart jenkins after 10 seconds to ensure that current job
+# won't fail.
+# Usually run on after_config tag by the end.
+
+set -e
+set -x
+
+(sleep 10 && sudo service jenkins restart)&
+
+exit 0
